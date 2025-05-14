5ae7a9b872206c2bb2d62cd4992ee3a3fb489513
Andres Reyes <areyesdev@gmail.com>
2025-05-13 21:44:40 -0500

    add shandcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Hola ecommerce TechStore</h1>
+      <Button variant="destructive">Click</Button>
     </div>
   );
 }
